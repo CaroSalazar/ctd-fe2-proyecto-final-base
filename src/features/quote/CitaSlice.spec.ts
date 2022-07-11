a54009701d60a843edb7e2d492ba0956a2f313e5
CaroSalazar <carolina97.salazar@gmail.com>
2022-07-10 23:57:09 -0300

    Funcionalidades obligatorias

---
+++ src/features/quote/CitaSlice.spec.ts
@@ -0,0 +1,33 @@
+import { ESTADO_FETCH } from "./constants";
+import citaReducer, { EstadoCita, limpiar } from "./citaSlice";
+
+describe("Reducer", () => {
+  const initialState: EstadoCita = {
+    data: null,
+    estado: ESTADO_FETCH.INACTIVO,
+  };
+  describe("as default", () => {
+    it("Debe devolver el estado inicial", () => {
+      const actual = citaReducer(initialState, { type: "any" });
+      expect(actual).toEqual(initialState);
+    });
+  });
+
+  describe("on clear", () => {
+    const initialState: EstadoCita = {
+      data: {
+        character: "personaje",
+        quote: "cita",
+        image: "imagen",
+        characterDirection: "direccion",
+      },
+      estado: ESTADO_FETCH.INACTIVO,
+    };
+    it("Deberia limpiar el estado", () => {
+      expect(citaReducer(initialState, { type: "citas/limpiar" })).toEqual({
+        data: null,
+        estado: ESTADO_FETCH.INACTIVO,
+      });
+    });
+  });
+});
